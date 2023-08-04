5c007c0f25a4b6b29c0e4805407992fe9efc1ab6
Michael Perdue <michael0perdue@gmail.com>
2023-08-04 22:50:50 +0100

    It now loads from csv files rather then text as data is stored more on csv, in addition also added the csv files for the conversion rates over the years of currencies to euros

--- src/main/java/graphing/LineCharts.java
+++ src/main/java/graphing/LineCharts.java
@@ -6,51 +6,58 @@ import javafx.scene.chart.XYChart;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class LineCharts {
-    private static int[] xMaxMin = new int[2];
-    private static int[] yMaxMin = new int[2];
+    private static double[] xMaxMin = new double[2];
+    private static double[] yMaxMin = new double[2];
     private static boolean unpopulated = true;
 
-    private static ArrayList<XYChart.Series> generateXYseries() throws FileNotFoundException {
+    private static ArrayList<XYChart.Series> generateXYseries() throws FileNotFoundException{
         ArrayList<XYChart.Series> series = new ArrayList<>();
         try {
             File[] files = new File("src/main/resources/years").listFiles();
-            for(File file : files){
-                XYChart.Series data = new XYChart.Series();
-                data.setName(file.getName());
-                Scanner scanner = new Scanner(file);
-                while (scanner.hasNextLine()) {
-                    String[] line = scanner.nextLine().split(" ");
-                    int[] numbers = {Integer.parseInt(line[0]), Integer.parseInt(line[1])};
-                    if (unpopulated) {
-                        Arrays.fill(xMaxMin, numbers[0]);
-                        Arrays.fill(yMaxMin, numbers[1]);
-                        unpopulated = false;
-                    }
-                    xMaxMin[0] = Math.min(numbers[0], xMaxMin[0]);
-                    xMaxMin[1] = Math.max(numbers[0], xMaxMin[1]);
-                    yMaxMin[0] = Math.min(numbers[1], yMaxMin[0]);
-                    yMaxMin[1] = Math.max(numbers[1], yMaxMin[1]);
-                    System.out.println(line[0] + line[1]);
-                    data.getData().add(new XYChart.Data<>(Integer.parseInt(line[0]), Integer.parseInt(line[1])));
+            for(File file : files) {
+                if (file.getName().contains(".csv")) {
+                    XYChart.Series data = new XYChart.Series();
+                    data.setName(file.getName());
+                    Scanner scanner = new Scanner(file);
+                    while (scanner.hasNextLine()) {
+                        String a = scanner.nextLine();
+                        String[] line = a.split(",");
+                        double[] numbers = {Double.parseDouble(line[0]), Double.parseDouble(line[1])};
+                        if (unpopulated) {
+                            Arrays.fill(xMaxMin, numbers[0]);
+                            Arrays.fill(yMaxMin, numbers[1]);
+                            unpopulated = false;
+                        }
+                        xMaxMin[0] = Math.min(numbers[0], xMaxMin[0]);
+                        xMaxMin[1] = Math.max(numbers[0], xMaxMin[1]);
+                        yMaxMin[0] = Math.min(numbers[1], yMaxMin[0]);
+                        yMaxMin[1] = Math.max(numbers[1], yMaxMin[1]);
+                        System.out.println(line[0] + line[1]);
+                        data.getData().add(new XYChart.Data<>(numbers[0],numbers[1]));
 
+                    }
+                    scanner.close();
+                    series.add(data);
                 }
-                scanner.close();
-                series.add(data);
             }
         }catch (Exception e){System.out.println("Error no files to process");throw e;}
         return series;
     }
-    public static LineChart<Number, Number> loadchart() throws FileNotFoundException {
+    public static LineChart<Number, Number> loadchart() throws FileNotFoundException{
         File file = new File("src/main/resources/years/data.txt");
         System.out.println(file.exists());
         ArrayList<XYChart.Series> series = generateXYseries();
-        NumberAxis xAxis = new NumberAxis(xMaxMin[0],xMaxMin[1],Math.round((double) (xMaxMin[1] - xMaxMin[0]) /30));
-        NumberAxis yAxis = new NumberAxis(yMaxMin[0],yMaxMin[1],Math.round((double) (yMaxMin[1] - yMaxMin[0]) /10));
+        double xstep = Math.ceil((xMaxMin[1] - xMaxMin[0]) /30) > 5 ? Math.ceil(((((xMaxMin[1] - xMaxMin[0]) / 30) + 5) /10)*10) : Math.ceil((xMaxMin[1] - xMaxMin[0]) /30);
+        double ystep = Math.ceil((yMaxMin[1] - yMaxMin[0]) /10) > 5 ? Math.ceil(((((yMaxMin[1] - yMaxMin[0]) /10) + 5) /10)*10) : Math.ceil((yMaxMin[1] - yMaxMin[0]) /10);
+        NumberAxis xAxis = new NumberAxis(Math.ceil(xMaxMin[0] - xstep),Math.floor(xMaxMin[1] + xstep),xstep);
+        NumberAxis yAxis = new NumberAxis(Math.ceil(yMaxMin[0]- ystep),Math.floor(yMaxMin[1] + ystep),ystep);
         xAxis.setLabel("x");
         yAxis.setLabel("y");
         LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);

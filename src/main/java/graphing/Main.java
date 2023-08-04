dc3a99e5f0259d7ed5c78ae9cb3914c2b96df21f
Michael Perdue <michael0perdue@gmail.com>
2023-08-04 07:38:16 +0100

    added a home screen page so the user has to choose to generate a linechart

--- src/main/java/graphing/Main.java
+++ src/main/java/graphing/Main.java
@@ -1,7 +1,12 @@
 package graphing;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -16,12 +21,24 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-
-        LineChart<Number, Number> lineChart = LineCharts.loadchart();
-        // display the line chart
-        Scene scene = new Scene(lineChart, 640, 480);
+        Button button = new Button("Generate linecharts");
+        StackPane tilePane = new StackPane();
+        EventHandler<ActionEvent> event = e -> {
+            try {
+                LineChart<Number, Number> lineChart = LineCharts.loadchart();
+                Scene scene = new Scene(lineChart, 640, 480);
+                Stage linechartStage = new Stage();
+                linechartStage.setScene(scene);
+                linechartStage.show();
+            }catch (Exception ex){ex.printStackTrace();}
+        };
+        button.setOnAction(event);
+        tilePane.getChildren().add(button);
+        Scene scene = new Scene(tilePane, 200, 200);
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
